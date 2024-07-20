9e456ac0aaba7dfd3a918bf6f1788b9ac35d0b06
thomasisa1 <thomasisa1@users.noreply.github.com>
2024-07-20 05:10:59 +0000

    registrationwithbettervalidationcheck

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.hybrid import hybrid_property
-from werkzeug.security import (
-    generate_password_hash, check_password_hash
-)
-db = SQLAlchemy()
+from werkzeug.security import generate_password_hash, check_password_hash
 
+db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = "users"
@@ -15,7 +13,7 @@ class User(db.Model):
     _password = db.Column(db.String(250), nullable=False)
     username = db.Column(db.String(80), nullable=False, unique=True)
     is_active = db.Column(db.Boolean(), nullable=False, default=True)
-    favorites = db.relationship("Favorite", back_populates="user", uselist=True)
+    favorites = db.relationship("Favorite", back_populates="user", cascade="all, delete-orphan")
 
     @hybrid_property
     def password(self):
@@ -25,43 +23,32 @@ class User(db.Model):
     def password(self, password):
         self._password = generate_password_hash(password)
 
-    def check_password_hash(self, other):
-        return check_password_hash(self._password, other)    
+    def check_password(self, password):
+        return check_password_hash(self._password, password)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
-
-    def serialize(self):
+    def to_dict(self):
         return {
             "id": self.id,
             "email": self.email,
             "first_name": self.first_name,
             "last_name": self.last_name,
             "username": self.username,
-            "is_active":self.is_active
-           
+            "is_active": self.is_active
         }
+
+    def from_dict(self, data, new_user=False):
+        for field in ['first_name', 'last_name', 'email', 'username']:
+            if field in data:
+                setattr(self, field, data[field])
+        if new_user and 'password' in data:
+            self.password = data['password']
 
 class Favorite(db.Model):
     __tablename__ = "favorites"
-    id = db.Column(
-        db.Integer, 
-        primary_key=True
-        )
-    user_id = db.Column(
-        db.Integer,
-        db.ForeignKey('users.id')
-    )
-    coin_id = db.Column(
-        db.String(80), 
-        nullable=False, 
-        unique=True
-        )
-    user = db.relationship(
-        "User",
-        back_populates="favorites",
-        uselist=False
-    )
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    coin_id = db.Column(db.String(80), nullable=False)
+    user = db.relationship("User", back_populates="favorites", uselist=False)
     
     def serialize(self):
         return {
