76ffa4b515aa60aab8c8512b0955129d75ccca45
Daniel Pena <163460072+daperk@users.noreply.github.com>
2024-08-06 22:43:33 +0000

    menu added

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,19 +1,24 @@
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container">
+        <Link className="navbar-brand" to="/">CryptoSense</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/dashboard">Dashboard</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/cryptocoins">Crypto Coins</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
